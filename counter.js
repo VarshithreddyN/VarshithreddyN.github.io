538a057159c2d4863adfa995362efe1825eef201
VarshithreddyN <nellireddyvarshith9853@gmail.com>
2023-01-18 22:18:58 +0530

    step 4
    changes in counter

--- counter.js
+++ counter.js
@@ -4,8 +4,13 @@ function updateCounter() {
   let count = localStorage.getItem('visiterCount');
   /**
    * if count is null then set count to 1
+   *
   * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
   */
+  if (count== null)
+    count='1';
+  else 
+    count=paraseInt(count)+1;
   localStorage.setItem('visiterCount', count);
   counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
 }
